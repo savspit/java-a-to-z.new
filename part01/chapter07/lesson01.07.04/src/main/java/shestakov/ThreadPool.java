0479e491478bcbbf4db99c4fa6b7ffe2632341b5
Anton Shestakov <savspit@gmail.com>
2016-07-22 10:16:51 +0300

    big project

---
+++ part01/chapter07/lesson01.07.04/src/main/java/shestakov/ThreadPool.java
@@ -0,0 +1,85 @@
+package shestakov;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+public class ThreadPool extends Thread{
+    private final LinkedList<Work> freeThreads;
+    private final List<Work> workingThreads;
+    private static final int DEFAULT_CAPACITY = Runtime.getRuntime().availableProcessors();
+    private boolean blockObj = false;
+
+    public ThreadPool(final int capacity) {
+        this.freeThreads = new LinkedList<>();
+        this.workingThreads = new ArrayList<>(capacity == 0 ? this.DEFAULT_CAPACITY : capacity);
+    }
+
+    public void add(Work work) {
+        synchronized (this) {
+            System.out.println(String.format("%s enable", Thread.currentThread().getId()));
+            this.freeThreads.add(work);
+            this.blockObj = true;
+            notifyAll();
+        }
+    }
+
+    public void runThreads() {
+        System.out.println(String.format("%s run threads", Thread.currentThread().getId()));
+        for (Work work : this.workingThreads) {
+            try {
+                work.run();
+            } catch (RuntimeException e) {
+                e.printStackTrace();
+            }
+        }
+        this.workingThreads.clear();
+    }
+
+    @Override
+    public void run() {
+        synchronized (this) {
+            while (this.blockObj) {
+                if (this.freeThreads.isEmpty()) {
+                    try {
+                        System.out.println(String.format("%s wait", Thread.currentThread().getId()));
+                        wait();
+                    } catch (InterruptedException e) {
+                        interrupt();
+                    }
+                }
+                System.out.println(String.format("%s usefull work", Thread.currentThread().getId()));
+                for (int i=0; i<this.freeThreads.size() && i<this.DEFAULT_CAPACITY; i++) {
+                    this.workingThreads.add(this.freeThreads.poll());
+                }
+                this.freeThreads.clear();
+            }
+            runThreads();
+        }
+        // get the rest of threads which need to be doing something
+        if (this.blockObj && !this.freeThreads.isEmpty()) {
+            synchronized (this) {
+                while (!this.freeThreads.isEmpty()) {
+                    for (int i = 0; i < this.freeThreads.size() && i < this.DEFAULT_CAPACITY; i++) {
+                        this.workingThreads.add(this.freeThreads.poll());
+                    }
+                    runThreads();
+                }
+                this.freeThreads.clear();
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        final ThreadPool pool = new ThreadPool(0);
+        pool.start();
+    }
+
+    public class Work extends Thread{
+        @Override
+        public void run() {
+            //todo some useful things
+        }
+    }
+
+}
