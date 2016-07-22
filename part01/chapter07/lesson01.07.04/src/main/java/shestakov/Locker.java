a0874288737485e5063feae1f959dbcddbfcd9ab
Anton Shestakov <savspit@gmail.com>
2016-07-22 10:44:52 +0300

    big project

---
+++ part01/chapter07/lesson01.07.04/src/main/java/shestakov/Locker.java
@@ -0,0 +1,56 @@
+package shestakov;
+
+public class Locker {
+    private boolean locked = false;
+
+    public void lock() {
+        synchronized (this) {
+            System.out.println(String.format("%s locked", Thread.currentThread().getId()));
+            doLock(true);
+        }
+    }
+
+    public void unlock() {
+        synchronized (this) {
+            System.out.println(String.format("%s unlocked", Thread.currentThread().getId()));
+            doLock(false);
+        }
+    }
+
+    public void doLock(boolean value) {
+        this.locked = value;
+        notifyAll();
+    }
+
+    public void doSomething() {
+        synchronized (this) {
+            while (this.locked) {
+                try {
+                    System.out.println(String.format("%s wait", Thread.currentThread().getId()));
+                    wait();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+            System.out.println(String.format("%s do", Thread.currentThread().getId()));
+        }
+    }
+
+    public static void main(String[] args) {
+        Locker locker = new Locker();
+        new Thread() {
+            @Override
+            public void run() {
+                locker.lock();
+                locker.doSomething();
+            }
+        }.start();
+        new Thread() {
+            @Override
+            public void run() {
+                locker.unlock();
+                locker.doSomething();
+            }
+        }.start();
+    }
+}
