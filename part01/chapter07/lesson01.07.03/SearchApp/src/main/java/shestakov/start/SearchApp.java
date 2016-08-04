b4c8ac7f5215018a6665e61fad1c06c9a6234949
Anton Shestakov <savspit@gmail.com>
2016-08-04 15:15:12 +0300

    big project

--- part01/chapter07/lesson01.07.03/SearchApp/src/main/java/shestakov/start/SearchApp.java
+++ part01/chapter07/lesson01.07.03/SearchApp/src/main/java/shestakov/start/SearchApp.java
@@ -1,5 +1,6 @@
 package shestakov.start;
 
+import org.apache.log4j.Logger;
 import shestakov.services.KeysValidator;
 import shestakov.threads.Worker;
 
@@ -8,9 +9,11 @@ import java.util.*;
 import java.util.concurrent.*;
 
 public class SearchApp {
+    private static final Logger log = Logger.getLogger(SearchApp.class);
     private KeysValidator kv;
     private Queue<File> fileTree = new PriorityQueue<>();
     private List<ScheduledFuture> results = new ArrayList<>();
+    private static final int DEFAULT_CAPACITY = Runtime.getRuntime().availableProcessors();
 
     public SearchApp(KeysValidator kv) {
         this.kv = kv;
@@ -22,18 +25,18 @@ public class SearchApp {
     }
 
     public void doSomeScheduledSearch() throws ExecutionException, InterruptedException {
-        ScheduledThreadPoolExecutor ex = new ScheduledThreadPoolExecutor(5);
+        ScheduledThreadPoolExecutor ex = new ScheduledThreadPoolExecutor(DEFAULT_CAPACITY);
         for (File currentFileOrDir : this.fileTree) {
             results.add(ex.schedule(new Worker(currentFileOrDir, this.kv.pattern), 1, TimeUnit.SECONDS));
         }
         for (ScheduledFuture result : this.results) {
             List<String> listOfMatches = (List<String>) result.get();
             if (listOfMatches.size() != 0 && this.kv.stopAtFirstResult) {
-                System.out.println(listOfMatches.get(0));
+                log.info(listOfMatches.get(0));
                 break;
             } else if (listOfMatches.size() != 0 && !this.kv.stopAtFirstResult) {
                 for (String match : listOfMatches) {
-                    System.out.println(match);
+                    log.info(match);
                 }
             }
         }
@@ -42,12 +45,11 @@ public class SearchApp {
 
     public void fillFileTree() {
         File[] paths;
-        try {
-            paths = File.listRoots();
-            for(File path:paths)
-            {
+        paths = File.listRoots();
+        for(File path : paths) {
+            if (path.canRead()) {
                 Collections.addAll(this.fileTree, path.listFiles());
             }
-        } catch (Exception ignore){ /*NOP*/ }
+        }
     }
 }
--- part01/chapter07/lesson01.07.03/SearchApp/src/main/java/shestakov/threads/Worker.java
+++ part01/chapter07/lesson01.07.03/SearchApp/src/main/java/shestakov/threads/Worker.java
@@ -1,11 +1,14 @@
 package shestakov.threads;
 
+import org.apache.log4j.Logger;
+
 import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.Callable;
 
 public class Worker implements Callable {
+    private static final Logger log = Logger.getLogger(Worker.class);
     File currentFileOrDir;
     String pattern;
     List<String> result = new ArrayList<>();
@@ -26,7 +29,9 @@ public class Worker implements Callable {
                 } else if (currentFileOrDir.isDirectory() && currentFileOrDir.list() != null) {
                     readDirectoryRec(currentFileOrDir);
                 }
-            } catch (SecurityException ignore) {/*NOP*/ }
+            } catch (SecurityException se) {
+                log.error("some kind of security exception occured", se);
+            }
         }
         return result;
     }
