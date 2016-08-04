627a5c1b0b9088539bd2a2795ae0f805a9a8a093
Anton Shestakov <savspit@gmail.com>
2016-08-04 15:56:53 +0300

    big project

--- part01/chapter07/lesson01.07.03/SearchApp/src/main/java/shestakov/start/StartUI.java
+++ part01/chapter07/lesson01.07.03/SearchApp/src/main/java/shestakov/start/StartUI.java
@@ -7,14 +7,8 @@ import java.util.concurrent.ExecutionException;
 public class StartUI {
 
     public static void main(String[] args) throws ExecutionException, InterruptedException {
-
-        String[] args1 = new String[3];
-        args1[0] = "-f";
-        args1[1] = "tmp";
-        args1[2] = "-s";
-
         KeysValidator kv = new KeysValidator();
-        if (kv.parsedSuccessful(args1)) {
+        if (kv.parsedSuccessful(args)) {
             SearchApp sa = new SearchApp(kv);
             new StartUI().init(sa);
         }
