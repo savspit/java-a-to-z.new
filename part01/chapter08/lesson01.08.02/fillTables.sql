125ddd69f482a4dd762e67c30b7e4a2547b08bff
Anton Shestakov <savspit@gmail.com>
2016-09-01 09:34:49 +0300

    big project

---
+++ part01/chapter08/lesson01.08.02/fillTables.sql
@@ -0,0 +1,54 @@
+﻿\connect mydatabase
+
+-- filterOperators
+INSERT INTO filterOperators(name) VALUES ('>');
+INSERT INTO filterOperators(name) VALUES ('<');
+INSERT INTO filterOperators(name) VALUES ('=');
+INSERT INTO filterOperators(name) VALUES ('>=');
+INSERT INTO filterOperators(name) VALUES ('<=');
+INSERT INTO filterOperators(name) VALUES ('<>');
+INSERT INTO filterOperators(name) VALUES ('IN');
+INSERT INTO filterOperators(name) VALUES ('LIKE');
+INSERT INTO filterOperators(name) VALUES ('NOT IN');
+INSERT INTO filterOperators(name) VALUES ('NOT LIKE');
+
+-- filterOperators
+INSERT INTO filterOperators(name) VALUES ('>');
+INSERT INTO filterOperators(name) VALUES ('<');
+
+-- filters
+INSERT INTO filters(
+            active,
+            extended,
+            name,
+            tableName,
+            fieldName,
+            fieldValue,
+            userId,
+            operationId) VALUES (
+            false,
+            "orderNumber = 1",
+            "orders",
+            "orderNumber",
+            1,
+            (SELECT id FROM users WHERE login LIKE '%admin%'),
+            (SELECT id FROM filterOperators WHERE name='=')
+);
+INSERT INTO filters(
+            active,
+            extended,
+            name,
+            tableName,
+            fieldName,
+            fieldValue,
+            userId,
+            operationId) VALUES (
+            false,
+            "orderComment like 1",
+            "orders",
+            "orderNumber",
+            1,
+            (SELECT id FROM users WHERE login LIKE '%user%'),
+            (SELECT id FROM filterOperators WHERE name='=')
+);
+
