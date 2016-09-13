373fc4af1494c605c5159980bcc8983fa7be13c4
Anton Shestakov <savspit@gmail.com>
2016-09-13 22:03:59 +0300

    big project

--- part01/chapter09/lesson01.09.01/MyWebProject/src/main/java/shestakov/servlets/EchoServlet.java
+++ part01/chapter09/lesson01.09.01/MyWebProject/src/main/java/shestakov/servlets/EchoServlet.java
@@ -9,18 +9,29 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
 
 /**
  * The type Echo servlet.
  */
 public class EchoServlet extends HttpServlet {
     private static final Logger Log = LoggerFactory.getLogger(EchoServlet.class);
+    private List<String> users = new CopyOnWriteArrayList<String>();
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         resp.setContentType("text/html");
+        String login = req.getParameter("login");
         PrintWriter writer = new PrintWriter(resp.getOutputStream());
-        writer.append("hello world.");
+        writer.append("hello world, " + this.users);
         writer.flush();
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html");
+        this.users.add(req.getParameter("login"));
+        doGet(req, resp);
     }
 }
