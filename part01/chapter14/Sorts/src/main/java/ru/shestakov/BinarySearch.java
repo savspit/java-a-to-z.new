53ff30292046e910d145879542ce47a76ed1afcf
Anton Shestakov <savspit@gmail.com>
2016-12-22 20:24:40 +0300

    big project

--- part01/chapter14/Sorts/src/main/java/ru/shestakov/BinarySearch.java
+++ part01/chapter14/Sorts/src/main/java/ru/shestakov/BinarySearch.java
@@ -21,10 +21,11 @@ public class BinarySearch<T extends Comparable<T>> {
 
     public boolean isSorted(T[] array) {
         if (array.length < 2) { return true; }
-        int sortingDirection = array[0].compareTo(array[1]);
+        int firstDirection = array[0].compareTo(array[1]);
         for (int i = 0; i < array.length - 1; i++) {
-            if ( (sortingDirection > 0 && array[i].compareTo(array[i+1]) < 0) ||
-                    (sortingDirection < 0 && array[i].compareTo(array[i+1]) > 0) ) {
+            int currentDirection = array[i].compareTo(array[i+1]);
+            if ( (firstDirection > 0 && currentDirection < 0) ||
+                    (firstDirection < 0 && currentDirection > 0) ) {
                 return false;
             }
         }
