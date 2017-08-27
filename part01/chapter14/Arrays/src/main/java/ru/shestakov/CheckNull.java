439e71f173a6be7ab87b93b7214eea969594fca8
Anton Shestakov <admin@sparox.ru>
2017-08-27 18:52:24 +0300

    big project

--- part01/chapter14/Arrays/src/main/java/ru/shestakov/CheckNull.java
+++ part01/chapter14/Arrays/src/main/java/ru/shestakov/CheckNull.java
@@ -2,26 +2,48 @@ package ru.shestakov;
 
 public class CheckNull {
 
-    public int[] clear(int[] arr) {
-        int[] newArr = new int[getNewSize(arr)];
-        int elemNum = 0;
+    public int[][] clear(int[][] arr) {
+        int [] stringsArr = new int [arr.length];
+        int [] columnsArr = new int [arr[0].length];
+
+        int countStr = 0;
+        int countCol = 0;
+
         for (int i=0; i<arr.length; i++) {
-            if (arr[i] != 0) {
-                newArr[elemNum] = arr[i];
-                elemNum++;
+            for (int j=0; j< arr[i].length; j++) {
+                if (arr[i][j] == 0) {
+                    stringsArr[countStr] = i;
+                    columnsArr[countCol] = j;
+                    countStr++;
+                    countCol++;
+                }
             }
         }
-        return newArr;
+        for (int i=countStr; i<stringsArr.length; i++) {
+            stringsArr[i] = Integer.MAX_VALUE;
+        }
+        for (int j=countCol; j<columnsArr.length; j++) {
+            columnsArr[j] = Integer.MAX_VALUE;
+        }
+
+        for (int k=0; k<stringsArr.length; k++) {
+            if (stringsArr[k] != Integer.MAX_VALUE) {
+                for (int j=0; j<arr[stringsArr[k]].length; j++) {
+                    arr[stringsArr[k]][j] = 0;
+                }
+            }
+        }
+
+        for (int m=0; m<columnsArr.length; m++) {
+            if (columnsArr[m] != Integer.MAX_VALUE) {
+                for (int i=0; i<arr.length; i++) {
+                    arr[i][columnsArr[m]] = 0;
+                }
+            }
+        }
+
+        return arr;
     }
 
-    private int getNewSize(int[] arr) {
-        int newSize = 0;
-        for (int i=0; i<arr.length; i++) {
-            if (arr[i] != 0) {
-                newSize++;
-            }
-        }
-        return newSize;
-    }
 
 }
--- part01/chapter14/Arrays/src/test/java/ru/shestakov/CheckNullTest.java
+++ part01/chapter14/Arrays/src/test/java/ru/shestakov/CheckNullTest.java
@@ -1,18 +1,37 @@
 package ru.shestakov;
 
-import org.junit.Assert;
 import org.junit.Test;
-
-import static org.hamcrest.core.Is.is;
 
 public class CheckNullTest {
 
     @Test
-    public void whenExistNullElementsShouldRemoveIt() {
-        int[] arr = {0, 1, 2, 3, 4, 5};
+    public void whenExistZeroElementsShouldZerosStringAndColumnIt() {
+        int[][] arr = getRandomArrayWithZeroElement(5, 5);
+        printArr(arr);
         CheckNull checkNull = new CheckNull();
-        int[] newArr = checkNull.clear(arr);
-        Assert.assertThat(newArr.length, is(arr.length-1));
+        int[][] newArr = checkNull.clear(arr);
+        printArr(newArr);
+    }
+
+    private void printArr(int[][] array) {
+        for (int i=0; i< array.length; i++) {
+            for (int j=0; j< array[i].length; j++) {
+                System.out.print(array[i][j]);
+            }
+            System.out.println("");
+        }
+        System.out.println("");
+    }
+
+    private int[][] getRandomArrayWithZeroElement(int str, int col) {
+        int[][] array = new int[str][col];
+        for (int i=0; i < array.length; i++){
+            for (int j=0; j < array[i].length; j++){
+                array[i][j] = (int)(Math.random() * 10);
+            }
+        }
+        array[array.length/2][array[0].length/2] = 0;
+        return array;
     }
 
 }
